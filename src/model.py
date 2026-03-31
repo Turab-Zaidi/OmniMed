3096fa4a3ff7e81a4dfd52ae3e59997376491dd1
Turab Zaidi <turab.z567@gmail.com>
2026-03-31 20:33:14 +0530

    changes made

--- src/model.py
+++ src/model.py
@@ -32,7 +32,7 @@ class OmniMedModel(nn.Module):
         self.llm = AutoModelForCausalLM.from_pretrained(
             model_id,
             quantization_config=bnb_config,
-            torch_dtype=torch.float16,
+            dtype=torch.float16,
             low_cpu_mem_usage=True,
             trust_remote_code=True
         )
